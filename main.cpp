05eb643636775e361824e35f9c80c4de5d976120
Alex Zhdanov <qwert2603@mail.ru>
2015-05-19 18:16:28 +0600

    _release 2.0

--- main.cpp
+++ main.cpp
@@ -1,107 +1,166 @@
-#include "stdafx.h"
-#include <iostream>
-#include <ctime>
 #include <windows.h>
-#include <glut.h>
 #include <vector>
 #include <map>
-#include "Aquarium.h"
-using namespace std;
+#include <string>
+#include <SFML\Graphics.hpp>
+#include "aquarium.h"
 
-double x_m = 1300, y_m = 650;
-double size = 10;
-Aquarium a1{ XMax(x_m), YMax(y_m), FishSize(size) };
+using std::map;
+using std::vector;
+using std::string;
+using std::pair;
 
-void circle(int x, int y, unsigned c)
-{
-	switch (c)
-	{
-	case 1: glColor3f(1, 0, 0); break;
-	case 2: glColor3f(0, 1, 0); break;
-	case 3: glColor3f(0, 0, 1); break;
-	case 4: glColor3f(0, 1, 1); break;
-	}
-	glBegin(GL_QUADS);
-	glVertex2f(x + size / 2, y + size / 2);
-	glVertex2f(x + size / 2, y - size / 2);
-	glVertex2f(x - size / 2, y - size / 2);
-	glVertex2f(x - size / 2, y + size / 2);
-	glEnd();
+// основные типы:
+using aquarium::Aquarium;				// класс аквариума
+using aquarium::FishType;				// класс типа рыб
+using aquarium::Fish;					// класс рыбы
+
+// для аквариума:
+using aquarium::XMax;					// класс ширины аквариума
+using aquarium::YMax;					// класс высоты аквариума
+using aquarium::FishSize;				// класс размера рыб
+
+// для типа рыб:
+using aquarium::FishTypeName;			// класс имени типа рыб
+using aquarium::SpeedWalk;				// класс скорости в спокойном состоянии
+using aquarium::SpeedRun;				// класс скорости во время погони
+using aquarium::Vision;					// класс радиуса зрения
+using aquarium::BirthFrequency;			// класс частоты рождения новых рыб
+using aquarium::Lifetime;				// класс продолжительности жизи
+using aquarium::MaxCount;				// класс максимального кол-ва рыб определенного типа
+using aquarium::Settled;				// класс, отвечающий за оседлость типа рыб
+
+// для одной рыбы:
+using aquarium::Location;				// класс местоположение
+using aquarium::X;						// класс координата х
+using aquarium::Y;						// класс координата у
+using aquarium::A;						// класс угол (в градусах)
+
+// для дипломатии:
+using aquarium::DiplomaticStatus;		// класс отношений между типами рыб
+
+
+// размеры аквариума
+const double x_m = 1300, y_m = 650;
+// размер рыбы
+const double fish_size = 10;
+
+
+namespace n1 {
+	std::vector<int> v;
 }
 
-void Draw()
-{
 
-	// обновление изобржения
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.0, 0.0, 0.0);
+int main() {
+	// сам аквариум
+	Aquarium a1{ XMax(x_m), YMax(y_m), FishSize(fish_size) };
 
-	unsigned c = 1;
-	auto koord = a1.locations();
-	for (const auto &a : koord) {
-		for (const auto &b : a.second) {
-			circle(b.x, b.y, c);
+	// добавляем типы рыб
+	a1.add_type(FishTypeName("main_hish"), SpeedWalk(7), SpeedRun(8.5), Vision(140), BirthFrequency(600), Lifetime(1200), MaxCount(200), Settled(false));
+	a1.add_type(FishTypeName("hish"), SpeedWalk(6), SpeedRun(8), Vision(110), BirthFrequency(270), Lifetime(800), MaxCount(400), Settled(false));
+	a1.add_type(FishTypeName("hish2"), SpeedWalk(6), SpeedRun(7.8), Vision(100), BirthFrequency(240), Lifetime(700), MaxCount(500), Settled(false));
+	a1.add_type(FishTypeName("trav"), SpeedWalk(5), SpeedRun(7.5), Vision(85), BirthFrequency(120), Lifetime(600), MaxCount(700), Settled(true));
+	a1.add_type(FishTypeName("trav2"), SpeedWalk(5), SpeedRun(7), Vision(70), BirthFrequency(90), Lifetime(500), MaxCount(900), Settled(false));
+	
+	// добавляем самих рыб
+	for (unsigned u = 0; u != 8; ++u) {
+		a1.add_fish(FishTypeName("main_hish"), Location(X(1000), Y(300), A(0)));
+		a1.add_fish(FishTypeName("hish"), Location(X(300), Y(400), A(90)));
+		a1.add_fish(FishTypeName("hish"), Location(X(300), Y(400), A(111)));
+		a1.add_fish(FishTypeName("hish"), Location(X(300), Y(400), A(140)));
+		a1.add_fish(FishTypeName("hish2"), Location(X(700), Y(400), A(180)));
+		a1.add_fish(FishTypeName("hish2"), Location(X(700), Y(400), A(160)));
+		a1.add_fish(FishTypeName("hish2"), Location(X(700), Y(300), A(0)));
+		a1.add_fish(FishTypeName("hish2"), Location(X(700), Y(500), A(0)));
+		a1.add_fish(FishTypeName("trav"), Location(X(900), Y(70), A(0)));
+		a1.add_fish(FishTypeName("trav"), Location(X(900), Y(60), A(0)));
+		a1.add_fish(FishTypeName("trav"), Location(X(900), Y(50), A(0)));
+		a1.add_fish(FishTypeName("trav"), Location(X(900), Y(40), A(0)));
+		a1.add_fish(FishTypeName("trav2"), Location(X(300), Y(200), A(0)));
+		a1.add_fish(FishTypeName("trav2"), Location(X(300), Y(210), A(0)));
+		a1.add_fish(FishTypeName("trav2"), Location(X(300), Y(220), A(0)));
+		a1.add_fish(FishTypeName("trav2"), Location(X(300), Y(230), A(0)));
+	}
+
+	// устанавливаем отношения между типами рыб
+	a1.set_diplomatic_status(FishTypeName("main_hish"), FishTypeName("hish"), DiplomaticStatus(2));
+	a1.set_diplomatic_status(FishTypeName("main_hish"), FishTypeName("hish2"), DiplomaticStatus(1));
+	a1.set_diplomatic_status(FishTypeName("hish"), FishTypeName("trav"), DiplomaticStatus(1));
+	a1.set_diplomatic_status(FishTypeName("hish2"), FishTypeName("trav2"), DiplomaticStatus(1));
+	a1.set_diplomatic_status(FishTypeName("hish"), FishTypeName("main_hish"), DiplomaticStatus(-1));
+	a1.set_diplomatic_status(FishTypeName("hish2"), FishTypeName("main_hish"), DiplomaticStatus(-1));
+	a1.set_diplomatic_status(FishTypeName("trav"), FishTypeName("hish"), DiplomaticStatus(-1));
+	a1.set_diplomatic_status(FishTypeName("trav2"), FishTypeName("hish2"), DiplomaticStatus(-1));
+	
+
+
+	//a1.add_type(FishTypeName("main_hish"), SpeedWalk(7), SpeedRun(8.5), Vision(240), BirthFrequency(600), Lifetime(1200), MaxCount(2), Settled(false));
+	//a1.add_type(FishTypeName("hish"), SpeedWalk(6), SpeedRun(8), Vision(210), BirthFrequency(320), Lifetime(800), MaxCount(5), Settled(false));
+	//a1.add_type(FishTypeName("trav"), SpeedWalk(5), SpeedRun(7.5), Vision(185), BirthFrequency(120), Lifetime(600), MaxCount(9), Settled(true));
+	//a1.add_fish(FishTypeName("main_hish"), Location(X(400), Y(300), A(0)));
+	//a1.add_fish(FishTypeName("hish"), Location(X(500), Y(300), A(0)));
+	//a1.add_fish(FishTypeName("trav"), Location(X(600), Y(300), A(0)));
+	//a1.set_diplomatic_status(FishTypeName("main_hish"), FishTypeName("hish"), DiplomaticStatus(2));
+	//a1.set_diplomatic_status(FishTypeName("hish"), FishTypeName("trav"), DiplomaticStatus(1));
+	//a1.set_diplomatic_status(FishTypeName("hish"), FishTypeName("main_hish"), DiplomaticStatus(-1));
+	//a1.set_diplomatic_status(FishTypeName("trav"), FishTypeName("hish"), DiplomaticStatus(-1));
+
+
+	// само окно 
+	sf::RenderWindow window(sf::VideoMode(x_m, y_m), "_aquarium");
+
+	while (window.isOpen()) {
+		// обработка закрытия окна
+		sf::Event Event;
+		while (window.pollEvent(Event)) {
+			if (Event.type == sf::Event::Closed)
+				window.close();
 		}
-		++c;
+
+		// рыбы делают шаг
+		a1.step();
+
+		// получаем местоположения рыб,
+		// а заодно и имена их типов, чтобы знать что рисовать
+		map<string, vector<Location>> fishes = a1.get_fish_locations();
+
+		// очистка старого изображения
+		window.clear(sf::Color::White);
+
+
+		// картинка рыбы (квадратик)
+		sf::RectangleShape fish_piture(sf::Vector2f(fish_size, fish_size));
+		// отображение рыб
+		for (const pair<string, vector<Location>> &one_type : fishes) {
+			// определяем с каким типом рыб мы имеем дело
+			// и выбираем соответствующий цвет
+			if (one_type.first == "main_hish") {
+				fish_piture.setFillColor(sf::Color::Black);
+			}
+			else if (one_type.first == "hish") {
+				fish_piture.setFillColor(sf::Color::Red);
+			}
+			else if (one_type.first == "hish2") {
+				fish_piture.setFillColor(sf::Color::Yellow);
+			}
+			else if (one_type.first == "trav") {
+				fish_piture.setFillColor(sf::Color::Green);
+			}
+			else if (one_type.first == "trav2") {
+				fish_piture.setFillColor(sf::Color::Blue);
+			}
+			// рисуем их
+			for (const Location &one_fish : one_type.second) {
+				fish_piture.setPosition(one_fish.x - fish_size / 2, y_m - (one_fish.y + fish_size / 2));
+				window.draw(fish_piture);
+			}
+		}
+
+		// отображение нового кадра
+		window.display();
+
+		// задержка времени
+		Sleep(25);
 	}
-
-	glFlush();
-}
-
-void timer(int = 0)
-{
-	a1.step();
-	cout << a1.current_time() << endl;
-
-	Draw();
-	glutTimerFunc(50, timer, 0);
-}
-
-int _tmain(int argc, char **argv)
-{
-	glutInit(&argc, argv);
-	srand(time(0));
-
-	a1.add_type(FishTypeName("hish"), SpeedWalk(6), SpeedRun(8), Vision(110), BirthFrequency(700), Lifetime(800), MaxCount(10), Settled(false));
-	a1.add_type(FishTypeName("hish2"), SpeedWalk(6), SpeedRun(8), Vision(110), BirthFrequency(700), Lifetime(800), MaxCount(10), Settled(false));
-	a1.add_type(FishTypeName("trav"), SpeedWalk(5), SpeedRun(7.5), Vision(85), BirthFrequency(100), Lifetime(600), MaxCount(10), Settled(true));
-	a1.add_type(FishTypeName("trav2"), SpeedWalk(5), SpeedRun(7), Vision(70), BirthFrequency(100), Lifetime(500), MaxCount(10), Settled(false));
-	
-	a1.add_fish(FishTypeName("hish"), Location(X(300), Y(300), A(0)));
-	a1.add_fish(FishTypeName("hish"), Location(X(500), Y(400), A(90)));
-	a1.add_fish(FishTypeName("hish2"), Location(X(300), Y(400), A(180)));
-	a1.add_fish(FishTypeName("hish2"), Location(X(100), Y(300), A(0)));
-	a1.add_fish(FishTypeName("hish"), Location(X(100), Y(500), A(0)));
-	a1.add_fish(FishTypeName("trav"), Location(X(200), Y(300), A(0)));
-	a1.add_fish(FishTypeName("trav"), Location(X(400), Y(400), A(0)));
-	a1.add_fish(FishTypeName("trav"), Location(X(300), Y(310), A(0)));
-	a1.add_fish(FishTypeName("trav2"), Location(X(400), Y(300), A(0)));
-	a1.add_fish(FishTypeName("trav2"), Location(X(500), Y(190), A(0)));
-
-	a1.set_diplomatic_status(FishTypeName("hish2"), FishTypeName("trav"), DiplomaticStatus(1));
-	a1.set_diplomatic_status(FishTypeName("hish2"), FishTypeName("trav2"), DiplomaticStatus(2));
-	a1.set_diplomatic_status(FishTypeName("hish"), FishTypeName("trav"), DiplomaticStatus(2));
-	a1.set_diplomatic_status(FishTypeName("hish"), FishTypeName("trav2"), DiplomaticStatus(1));
-	a1.set_diplomatic_status(FishTypeName("trav"), FishTypeName("hish"), DiplomaticStatus(-2));
-	a1.set_diplomatic_status(FishTypeName("trav"), FishTypeName("hish2"), DiplomaticStatus(-1));
-	a1.set_diplomatic_status(FishTypeName("trav2"), FishTypeName("hish2"), DiplomaticStatus(-2));
-	a1.set_diplomatic_status(FishTypeName("trav2"), FishTypeName("hish"), DiplomaticStatus(-1));
-	
-
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(x_m, y_m);
-	glutInitWindowPosition(20, 50);
-	glutCreateWindow("Аквариум");
-
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, x_m, 0, y_m, 0, 1);
-
-	glutDisplayFunc(Draw);
-	timer();
-	glutMainLoop();
-
 	return 0;
 }
-
